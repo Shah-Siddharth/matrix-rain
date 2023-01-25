18116dbff990e7214e13f1f0af3476fce364a531
Siddharth <siddharthshah1102@gmail.com>
2023-01-25 16:23:24 +0530

    main animation done

--- script.js
+++ script.js
@@ -2,3 +2,45 @@ const canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
+
+class Symbol {
+    constructor(x, y, fontSize, canvasHeight) {
+        this.characters = "アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        this.x = x;
+        this.y = y;
+        this.fontSize = fontSize;
+        this.canvasHeight = canvasHeight;
+    }
+
+    draw(context) {
+        this.char = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
+        context.fillStyle = "#0aff0a";
+        context.font = this.fontSize + "px monospace";
+        context.fillText(this.char, this.x * this.fontSize, this.y * this.fontSize);
+        this.y += 1;
+        
+        if(this.y * this.fontSize > this.canvasHeight) this.y = 0;
+    }
+}
+
+class Effect {
+    constructor(canvasWidth, canvasHeight, fontSize) {
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+        this.fontSize = fontSize;
+        this.columns = this.canvasWidth / this.fontSize;
+        
+        this.symbols = Array.from({length: this.columns}, (elem, i) => {
+            return new Symbol(i, 0, this.fontSize, this.canvasHeight);
+        });
+    }
+}
+
+const effect = new Effect(canvas.width, canvas.height, 25);
+
+function animate() {
+    effect.symbols.forEach(symbol => symbol.draw(ctx));
+    requestAnimationFrame(animate);
+}
+
+//animate();
